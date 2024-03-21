9df393c14fe14947b84ebecb5a7fd8ae7ee0aac0
VerselT <Verseltristan@gmail.com>
2024-03-21 15:42:25 +0100

    PJ + ouverture mail

--- src/main/java/com/example/mailer/ControllerMailer.java
+++ src/main/java/com/example/mailer/ControllerMailer.java
@@ -2,8 +2,14 @@ package com.example.mailer;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -18,11 +24,31 @@ public class ControllerMailer implements Initializable {
     private VBox openMsgVbox;
     @FXML
     private VBox mailList;
+    @FXML
+    private Label titreMessage;
+    @FXML
+    private Label senderMessage;
+    @FXML
+    private Label msgMessage;
+    @FXML
+    private Button btnPJ;
+    @FXML
+    private HBox fileContainer;
+    @FXML
+    private Label fileName;
+    private File PJ;
 
     private List<Mail> mails = new ArrayList<>();
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Remplir la liste des courriers électroniques
+        mails.add(new Mail("Mise a jour des informations","Jonh Newman", "Une interface web est une interface homme-machine constituée de pages web et permettant dans certains cas d'utiliser des applications web.\n" +
+                "\n" +
+                "Un client ou plus populairement un navigateur web tel que Firefox, Safari, Google Chrome, Internet Explorer ou bien Opera étant installable et généralement présent sur tout ordinateur moderne, une interface web est visualisable à partir de n'importe quel dispositif possédant un navigateur web (ordinateur, tablette ou smartphone, etc.). Elle est aussi potentiellement accessible du monde entier grâce à l'Internet.\n" +
+                "\n" +
+                "Pour choisir le contenu de la page du navigateur il faut entrer une URL.\n" +
+                "\n" +
+                "De nombreux matériels tels que routeur, modem ou photocopieur disposent d'une interface web permettant de les administrer."));
         for(int i=0; i<30;i++) {
             mails.add(new Mail("titre" + i,"sender","cocuouc"));
         }
@@ -30,8 +56,29 @@ public class ControllerMailer implements Initializable {
             VBox mailBox = createMailBox(mail);
             mailBox.getStyleClass().add("mailBox");
             mailList.getChildren().add(mailBox);
-
         }
+
+        btnPJ.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Choisir un fichier");
+            File selectedFile = fileChooser.showOpenDialog(new Stage());
+            String filename;
+            if (selectedFile != null) {
+                PJ = selectedFile;
+                filename = selectedFile.getName();
+                String extension = "";
+                int lastIndexOfDot = filename.lastIndexOf(".");
+                if (lastIndexOfDot != -1) {
+                    extension = filename.substring(lastIndexOfDot + 1);
+                }
+            }
+            if(PJ != null){
+                filename = PJ.getName();
+                fileContainer.setVisible(true);
+                fileName.setText(filename);
+            }
+        });
+
     }
 
     private VBox createMailBox(Mail mail) {
@@ -68,8 +115,13 @@ public class ControllerMailer implements Initializable {
             newMsgVbox.setManaged(newMsgVbox.isVisible());
             openMsgVbox.setVisible(!openMsgVbox.isVisible());
             openMsgVbox.setManaged(openMsgVbox.isVisible());
+            titreMessage.setText(mail.getTitle());
+            senderMessage.setText(mail.getSender());
+            msgMessage.setText(mail.getMessage());
         }else{ //maj infos
-
+            titreMessage.setText(mail.getTitle());
+            senderMessage.setText(mail.getSender());
+            msgMessage.setText(mail.getMessage());
         }
     }
 }
